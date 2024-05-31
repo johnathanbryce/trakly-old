b6969e6cc8eabee1e91b7f07583d50d990c772c8
johnathanbryce <johnathanbryce@rompathlete.com>
2024-05-31 09:51:30 -0700

    initial project and basic data fetching setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
