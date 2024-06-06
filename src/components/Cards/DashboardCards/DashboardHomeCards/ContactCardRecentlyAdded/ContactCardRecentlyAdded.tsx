10e0326a1929369c6eca1c48b6bbea33a31239e4
johnathanbryce <johnathanbryce@rompathlete.com>
2024-06-05 17:22:52 -0700

    feat: update color schemes & minor css improvements to dashboard home cards

--- src/components/Cards/DashboardCards/DashboardHomeCards/ContactCardRecentlyAdded/ContactCardRecentlyAdded.tsx
+++ src/components/Cards/DashboardCards/DashboardHomeCards/ContactCardRecentlyAdded/ContactCardRecentlyAdded.tsx
@@ -24,24 +24,26 @@ export default function ContactCardRecentlyAdded({
   return (
     <article className={styles.card_recent}>
         <IconTrash className={styles.icon_delete} size={iconSize} onClick={handleDeleteItem}/>
-        <p className={styles.name}>{`${firstName} ${lastName ? lastName : ''}`}</p>
+        <div className={styles.card_top_flex_container}>
+          <p className={styles.name}>{`${firstName} ${lastName ? lastName : ''}`}</p>
 
-        <div>
-          <p> {position} {companyAnyPositioBracket} {company}</p>
+          <div>
+            <p className={styles.position_and_company_text}> {position} {companyAnyPositioBracket} {company}</p>
+          </div>
+          
+          <p className={styles.contact}> 
+              {email} {contactBracket} {phone}
+          </p>
         </div>
-        
-        <p className={styles.contact}> 
-            {email} {contactBracket} {phone}
-        </p>
 
         <div className={styles.card_footer}>
-         <p className={styles.date_added}><span className={styles.added_text}>Added: </span>{createdAt.toString()}</p>
-         <div className={styles.icons_container}>
-            {links?.linkedIn && <Link href={links?.linkedIn} target="_blank"><IconBrandLinkedin className={styles.icon_contact} size={iconSize}/></Link>}
-            {links?.website && <Link href={links?.website} target="_blank"><IconWorldWww className={styles.icon_contact} size={iconSize}/></Link>}
-            {phone && <a href={`tel:${phone}`}><IconPhone className={styles.icon_contact} size={iconSize}/></a>}
-            {email && <a href={`mailtol:${email}`} target="_blank"><IconMail className={styles.icon_contact} size={iconSize}/></a>}
-         </div>
+          <p className={styles.date_added}><span className={styles.added_text}>Added: </span>{createdAt.toString()}</p>
+          <div className={styles.icons_container}>
+              {links?.linkedIn && <Link href={links?.linkedIn} target="_blank"><IconBrandLinkedin className={styles.icon_contact} size={iconSize}/></Link>}
+              {links?.website && <Link href={links?.website} target="_blank"><IconWorldWww className={styles.icon_contact} size={iconSize}/></Link>}
+              {phone && <a href={`tel:${phone}`}><IconPhone className={styles.icon_contact} size={iconSize}/></a>}
+              {email && <a href={`mailtol:${email}`} target="_blank"><IconMail className={styles.icon_contact} size={iconSize}/></a>}
+          </div>
         </div>
     </article>
   )
