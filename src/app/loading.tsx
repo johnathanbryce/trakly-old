4f234fddcea5b234be0158243ace11a7081a37e8
johnathanbryce <johnathanbryce@rompathlete.com>
2024-06-19 11:51:57 -0700

    feat: created custom loading spinner component

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import LoaderSpinner from "@/components/Loaders/LoaderSpinner/LoaderSpinner";
+
+
+const Loading = () => {
+    return <LoaderSpinner />
+  };
+  
+  export default Loading;
--- src/components/Dashboard/DashboardHome/DashboardHomeRecentContacts/DashboardHomeRecentContacts.tsx
+++ src/components/Dashboard/DashboardHome/DashboardHomeRecentContacts/DashboardHomeRecentContacts.tsx
@@ -1,6 +1,8 @@
 'use client'
+import { Suspense } from 'react';
 // Interal Components
 import ContactCardRecentlyAdded from '@/components/Cards/DashboardCards/DashboardHomeCards/ContactCardRecentlyAdded/ContactCardRecentlyAdded'
+import LoaderSpinner from '@/components/Loaders/LoaderSpinner/LoaderSpinner';
 // Custom Hooks
 import { useFetchData } from '@/hooks/useFetchData';
 // Recoil State
@@ -12,14 +14,14 @@ export default function DashboardHomeRecentContacts() {
     const { data: contacts, error, loading } = useFetchData<Contact[]>(`http://localhost:8000/api/contacts?limit=5`, contactsState); 
 
     if (loading) {
-        return <p>Loading contacts...</p>;
+        return <LoaderSpinner />;
     }
     
     if (error) {
         return <h1>Error fetching contacts data... please try again</h1>;
     }
     
-    if (!contacts || contacts.length === 0) {
+    if (contacts && contacts.length === 0) {
         return (
             <div>
                 <p> Hmm, looks like you haven&apos;t added any contacts yet...</p>
@@ -29,24 +31,24 @@ export default function DashboardHomeRecentContacts() {
     }
 
   return (
-        <>
-            {contacts.map((contact) =>
-                <ContactCardRecentlyAdded
-                    contact_id={contact.contact_id}
-                    key={contact.contact_id} 
-                    first_name={contact.first_name}
-                    last_name={contact.last_name}
-                    position={contact.position}
-                    company={contact.company}
-                    email={contact.email}
-                    phone={contact.phone}
-                    github={contact.github}
-                    instagram={contact.instagram}
-                    website={contact.website}
-                    linkedin={contact.linkedin}
-                    created_at={contact.created_at}
-                />
-            )}
-        </>
+    <>
+        {contacts && contacts.map((contact) => (
+            <ContactCardRecentlyAdded
+                contact_id={contact.contact_id}
+                key={contact.contact_id} 
+                first_name={contact.first_name}
+                last_name={contact.last_name}
+                position={contact.position}
+                company={contact.company}
+                email={contact.email}
+                phone={contact.phone}
+                github={contact.github}
+                instagram={contact.instagram}
+                website={contact.website}
+                linkedin={contact.linkedin}
+                created_at={contact.created_at}
+            />
+        ))}
+    </>
   )
 }
