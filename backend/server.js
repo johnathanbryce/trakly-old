989d7ef6ab036b1f16d44300205de350f8b5882d
johnathanbryce <johnathanbryce@rompathlete.com>
2024-06-20 16:08:39 -0700

    update: updating Clerk auth back into workflow

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require('body-parser')
 const cors = require("cors");
+// Clerk / Auth
+const { requireSession } = require('@clerk/clerk-sdk-node');
+const { ClerkExpressWithAuth } = require('@clerk/clerk-sdk-node/express')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,6 +13,17 @@ const PORT = process.env.PORT || 8080;
 // routes import
 const contactsRouter = require('./routes/contacts');
 const usersRouter = require('./routes/users');
+
+// Use Clerk middleware
+app.use(ClerkExpressWithAuth({
+  apiKey: process.env.CLERK_API_KEY
+}));
+
+// Example protected route
+app.get('/protected', requireSession, (req, res) => {
+  const userId = req.auth.userId;
+  res.send(`Hello, user ${userId}`);
+});
 
 // middleware to parse JSON bodies
 app.use(express.json()); // built-in body parser
