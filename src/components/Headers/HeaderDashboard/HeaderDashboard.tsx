1d0389268b6389d258a2e554ee23b5a1dec81fd7
johnathanbryce <johnathanbryce@rompathlete.com>
2024-06-26 11:27:47 -0700

    feat: added theme toggle, updates to overall colour schemes, added delete fn to message templates

--- src/components/Headers/HeaderDashboard/HeaderDashboard.tsx
+++ src/components/Headers/HeaderDashboard/HeaderDashboard.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import styles from './HeaderDashboard.module.css'
 // Internal Components
 import Button from '@/components/Buttons/Button/Button';
+import ThemeToggle from '../ThemeToggle/ThemeToggle';
 // Clerk Auth
 import { SignOutButton } from '@clerk/nextjs';
 
@@ -17,11 +18,15 @@ export default function HeaderDashboard() {
   return (
     <header className={styles.header}>
         <h2> Trakly</h2>
-        {isClient && (
-        <SignOutButton>
-           <Button label='Log out' isLoading={false} />
-        </SignOutButton>
-      )}
+        <div className={styles.header_buttons_container}>
+          <ThemeToggle />
+          {isClient && (
+          <SignOutButton>
+            <Button label='Log out' isLoading={false} />
+          </SignOutButton>
+        )}
+        </div>
+        
     </header>
   )
 }
